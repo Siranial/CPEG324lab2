34402f9e2745cba05f8a54075be2d021dd956251
Siranial <jonadams@udel.edu>
2022-04-04 15:09:44 -0400

    Revert "Revert "changed demux to output vector and now only selects one register""
    This reverts commit 18a391447d9173d255af938a3d97e87a7904ddc9.

--- lab2demux.vhdl
+++ lab2demux.vhdl
@@ -4,14 +4,8 @@ use IEEE.STD_LOGIC_1164.ALL;
 entity demux8 is
     Port ( I : in std_logic;
            S : in std_logic_vector (2 downto 0);
-           Y0 : out std_logic;
-           Y1 : out std_logic;
-           Y2 : out std_logic;
-           Y3 : out std_logic;
-           Y4 : out std_logic;
-           Y5 : out std_logic;
-           Y6 : out std_logic;
-           Y7 : out std_logic);
+           Y : out std_logic_vector (7 downto 0) := (others=>'0')
+           );
 end demux8;
 
 architecture Behavioral of demux8 is
@@ -19,24 +13,17 @@ architecture Behavioral of demux8 is
 begin
     process(I,S) is
     begin
+        -- Ensures that only one register is writing
+        Y <= "00000000";
         case S is
-            when "000" => Y0 <= I;
-            when "001" => Y1 <= I;
-            when "010" => Y2 <= I;
-            when "011" => Y3 <= I;
-            when "100" => Y4 <= I;
-            when "101" => Y5 <= I;
-            when "110" => Y6 <= I;
-            when others => Y7 <= I;
+            when "000" => Y(0) <= I;
+            when "001" => Y(1) <= I;
+            when "010" => Y(2) <= I;
+            when "011" => Y(3) <= I;
+            when "100" => Y(4) <= I;
+            when "101" => Y(5) <= I;
+            when "110" => Y(6) <= I;
+            when others => Y(7) <= I;
         end case;
     end process;
-
---    Y0 <= (not S(2) and not S(1) and not S(0)) and I;
---    Y0 <= (not S(2) and not S(1) and S(0)) and I;
---    Y0 <= (not S(2) and S(1) and not S(0)) and I;
---    Y0 <= (not S(2) and S(1) and S(0)) and I;
---    Y0 <= (S(2) and not S(1) and not S(0)) and I;
---    Y0 <= (S(2) and not S(1) and S(0)) and I;
---    Y0 <= (S(2) and S(1) and not S(0)) and I;
---    Y0 <= (S(2) and S(1) and S(0)) and I;
 end Behavioral;
