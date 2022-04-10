0a065b674c9a2512719df2fc72f7339a0c396099
Siranial <jonadams@udel.edu>
2022-04-10 16:10:36 -0400

    Changed mux to use generics for variable data width

--- Part1/mux.vhdl
+++ Part1/mux.vhdl
@@ -2,18 +2,22 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
 entity mux is
-    Port ( I : in STD_LOGIC_VECTOR (3 downto 0);
-           S : in STD_LOGIC_VECTOR (1 downto 0);
-           Y : out STD_LOGIC);
+    generic(dataWidth : integer);
+    Port (  I0 : in STD_LOGIC_VECTOR (dataWidth-1 downto 0);
+            I1 : in STD_LOGIC_VECTOR (dataWidth-1 downto 0);
+            I2 : in STD_LOGIC_VECTOR (dataWidth-1 downto 0);
+            I3 : in STD_LOGIC_VECTOR (dataWidth-1 downto 0);
+            S : in STD_LOGIC_VECTOR (1 downto 0);
+            Y : out STD_LOGIC_vector (dataWidth-1 downto 0));
 end mux;
 
 architecture Behavioral of mux is
 
 begin
     with S select
-    Y<= I(0) when "00",
-        I(1) when "01",
-        I(2) when "10",
-        I(3) when others;
+    Y<= I0 when "00",
+        I1 when "01",
+        I2 when "10",
+        I3 when others;
 
 end Behavioral;
--- Part1/muxTB.vhdl
+++ Part1/muxTB.vhdl
@@ -7,38 +7,62 @@ end testbench;
 
 architecture Behavioral of testbench is
    
-    COMPONENT mux 
-    PORT(
-        I: IN std_logic_vector (3 downto 0);
-        S: IN std_logic_vector (1 downto 0);
-        Y: OUT std_logic
-    );
-    END COMPONENT;
+    component mux is
+        generic(dataWidth : integer);
+        Port (  I0 : in STD_LOGIC_VECTOR (dataWidth-1 downto 0);
+                I1 : in STD_LOGIC_VECTOR (dataWidth-1 downto 0);
+                I2 : in STD_LOGIC_VECTOR (dataWidth-1 downto 0);
+                I3 : in STD_LOGIC_VECTOR (dataWidth-1 downto 0);
+                S : in STD_LOGIC_VECTOR (1 downto 0);
+                Y : out STD_LOGIC_vector (dataWidth-1 downto 0));
+    end component;
     
-    signal I : std_logic_vector (3 downto 0) := (others=>'0');
+    signal I : std_logic_vector (7 downto 0) := (others=>'0');
     signal S : std_logic_vector (1 downto 0) := (others=>'0');
-    signal Y : std_logic;
+    signal Y : std_logic_vector (1 downto 0) := (others=>'0');
 
 begin
-    uut: mux PORT MAP (
-          I => I,
-          S => S,
-          Y => Y
+    uut: mux
+    generic map(dataWidth => 2)
+    port map (
+        I0 => I(1 downto 0),
+        I1 => I(3 downto 2),
+        I2 => I(5 downto 4),
+        I3 => I(7 downto 6),
+        S => S,
+        Y => Y
         );
-    stim_proc: process
-    begin
-        wait for 100 ns;
-        
-        I<="1001";
-        
-        S<="00";
-        wait for 100 ns;
-        S<="01";
-        wait for 100 ns;
-        S<="10";
-        wait for 100 ns;
-        S<="11";
-        wait for 100 ns; 
-        wait;-- Wait forever to finish simulation
-    end process;
+
+    --  Test predicted outputs
+process
+type pattern_type is record
+--  The inputs of the reg.
+    i: std_logic_vector (7 downto 0);
+	s: std_logic_vector (1 downto 0);
+--  The expected outputs of the reg.
+	y: std_logic_vector (1 downto 0);
+end record;
+--  The patterns to apply.
+type pattern_array is array (natural range <>) of pattern_type;
+constant patterns : pattern_array :=
+(("11000110", "00", "10"),
+("11000110", "11", "11"),
+("11000110", "10", "00"),
+("11000110", "01", "01"));
+begin
+--  Check each pattern.
+	for n in patterns'range loop
+--  Set the inputs.
+		i <= patterns(n).i;
+		s <= patterns(n).s;
+--  Wait for the results.
+		wait for 1 ns;
+--  Check the outputs.
+		assert y = patterns(n).y
+		report "bad output value" severity error;
+	end loop;
+	assert false report "end of test" severity note;
+--  Wait forever; this will finish the simulation.
+	wait;
+end process;
 end Behavioral;
